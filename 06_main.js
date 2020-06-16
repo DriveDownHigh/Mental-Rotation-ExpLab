08bba1a4c7b6781ca74236d5dda0150deeef54cc
DriveDownHigh <62028515+DriveDownHigh@users.noreply.github.com>
2020-06-16 16:08:53 +0200

    Update 06_main.js

--- 06_main.js
+++ 06_main.js
@@ -25,13 +25,13 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
-            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
+            experimentID: "95",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/95",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
+            deployMethod: "directLink",
+            contact_email: "j.katarina@mail.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
         // Here, you can specify how the progress bar should look like
